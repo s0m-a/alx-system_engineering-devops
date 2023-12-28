e5c76f8a65ff95ae1891cbb89d15d5b73162b37e
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-12-27 23:40:32 -0800

    updates

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/school':
+    ensure  => 'file',
+    path    => '/tmp/school',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
