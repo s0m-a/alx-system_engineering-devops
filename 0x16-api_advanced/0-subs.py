ed6cc959e28d6a569af3babd8942b0ac17c80dd0
soma <ogbennaanita.com>
2024-02-13 20:47:41 -0800

    updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,12 @@ def number_of_subscribers(subreddit):
     Queries  and returns the number of
     subscribers for a given subreddit.
     """
-    headers = {'User-Agent': 'Reddit API'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    return 0
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
